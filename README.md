5d2cdc08f60855cbaa417011734a758d970ee8a6
Martijn de Geus <martijn@side-shore.nl>
2019-06-24 22:03:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SaaS (Software as a Service)
+
+Processwire module to restrict pages by a shared saas_id.
+
+### Functionality
+
+A saas_id field is created, and added to the User-template.
+Add saas_id field to templates, by configuring this module.
+If the saas_id of the user, and the saas_id of the page match, access is granted.
+
+#### Support forum:
+
+
+## License
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+(See included LICENSE file for full license text.)
---
+++ Saas.module.php
@@ -0,0 +1,296 @@
+<?php namespace ProcessWire;
+
+/**
+ *
+ * Saas
+ *
+ * @author Martijn de Geus
+ *
+ * ProcessWire 3.x
+ * Copyright (C) 2011 by Ryan Cramer
+ * Licensed under GNU/GPL v2, see LICENSE.TXT
+ *
+ * http://www.processwire.com
+ * http://www.ryancramer.com
+ *
+ */
+
+class Saas extends WireData implements Module, ConfigurableModule {
+
+	/**
+	 * Module information
+	 */
+	public static function getModuleInfo() {
+		return array(
+			'title' => 'Saas',
+			'summary' => 'Restict acces to Pages by Saas-id.',
+			'version' => '0.0.1',
+			'author' => 'Martijn de Geus',
+			'href' => 'https://github.com/spoetnik/SaaS',
+			'icon' => 'key',
+			'autoload' => true,
+			'requires' => 'ProcessWire>=3.0.0',
+		);
+	}
+
+	/**
+	 * Construct
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->no_access_action = 0;
+		$this->replacement_title = 'Access denied';
+		$this->replacement_message = 'You don\'t have permissions to access this page.';
+	}
+
+	/**
+	 * Ready
+	 */
+	public function ready() {
+		// Check the saas_id on configured saas_templates
+		if(!$this->wire('user')->isSuperuser() && in_array($this->wire('page')->template->name, $this->saas_templates) ) {
+			$this->addHookAfter('Page::render', $this, 'checkAccess');
+		}
+
+        // Add the saas_id on page save
+		$this->addHookBefore('Pages::saveReady', $this, 'restrictAccess');
+	}
+
+	/**
+	 * Check if rendered page may be accessed
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function checkAccess(HookEvent $event) {
+
+		/* @var Page $page */
+		$page = $event->object;
+		/* @var User $user */
+		$user = $this->wire('user');
+
+		// Return if action is throw 404 and this is the 404 page
+		if($this->no_access_action == 1 && $page->id === $this->wire('config')->http404PageID) return;
+
+		// Match pages saas_id with users saas_id
+		$matches = false;
+		if($page->saas_id == $user->saas_id){
+			$matches = true;
+		}
+		// Return if page matches user's saas_id
+		if($matches) return;
+
+		wire('log')->save('saas', 'Access Denied' );
+		// Throw a 404 if this action is selected in the config
+		if($this->no_access_action == 1) throw new Wire404Exception();
+
+		// Replace the page markup
+		$event->return = $this->replacementMarkup($page, $this->replacement_title, $this->replacement_message);
+	}
+
+	/**
+	 * Get replacement markup to render
+	 *
+	 * @param Page $page The page that will have its rendered markup replaced
+	 * @param string $title The title defined in the module config
+	 * @param string $message The message defined in the module config
+	 * @return string
+	 */
+	public function ___replacementMarkup($page, $title, $message) {
+		$out = <<<EOD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>$title</title>
+</head>
+<body style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; padding:20px;">
+$message
+</body>
+</html>
+EOD;
+		return $out;
+	}
+
+	/**
+	 * Add the saas_id to the saved page
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function restrictAccess(HookEvent $event) {
+		/* @var Page $page */
+		$page = $event->arguments(0);
+		/* @var User $user */
+		$user = $this->wire('user');
+
+		// If the page has a saas_id field, give it the value of the users saas_id.
+		if($page->hasField(saas_id)) {
+			$page->saas_id = $user->saas_id;
+		}
+
+	}
+
+	/**
+	 * Config inputfields
+	 *
+	 * @param InputfieldWrapper $inputfields
+	 */
+	public function getModuleConfigInputfields($inputfields) {
+		$modules = $this->wire('modules');
+		$tmpTemplates = wire('templates');
+
+		foreach($tmpTemplates as $template) { // exclude system templates
+			if($template->flags & Template::flagSystem) continue;
+			$templates[] = $template;
+        }
+
+		/* @var InputfieldAsmSelect $f */
+		$f = $modules->get("InputfieldAsmSelect");
+		$f_name = 'saas_templates';
+		$f->name = $f_name;
+		$f->label = $this->_('Templates restricted by SaaS');
+		$f->description = $this->_('Choose the templates for wich content will be restricted by SaaS');
+        foreach($templates as $template) $f->addOption($template->name);
+		$f->value = $this->$f_name;
+        $inputfields->add($f);
+
+		/* @var InputfieldRadios $f */
+		$f = $modules->InputfieldRadios;
+		$f_name = 'no_access_action';
+		$f->name = $f_name;
+		$f->label = $this->_('Action to take if the visitor may not access the rendered page');
+		$f->addOption(0, $this->_('Replace the rendered markup'));
+		$f->addOption(1, $this->_('Throw a 404 exception'));
+		$f->value = $this->$f_name;
+		$inputfields->add($f);
+
+		/* @var InputfieldFieldset $f */
+		$fs = $this->wire('modules')->InputfieldFieldset;
+		$fs->label = $this->_('Replacement markup');
+		$fs->description .= $this->_('If you need more advanced markup than is possible with the options below then you can hook `AccessByQueryString::replacementMarkup()`.');
+		$fs->showIf = 'no_access_action=0';
+
+		/* @var InputfieldText $f */
+		$f = $this->wire('modules')->InputfieldText;
+		$f_name = 'replacement_title';
+		$f->name = $f_name;
+		$f->label = $this->_('Meta title');
+		$f->value = $this->$f_name;
+		$fs->add($f);
+
+		/* @var InputfieldTextarea $f */
+		$f = $this->wire('modules')->InputfieldTextarea;
+		$f_name = 'replacement_message';
+		$f->name = $f_name;
+		$f->label = $this->_('Message');
+		$f->value = $this->$f_name;
+		$f->rows = 3;
+		$fs->add($f);
+
+		$inputfields->add($fs);
+
+		$modules->addHookAfter('saveConfig', $this, 'afterConfigSave');
+
+	}
+
+	/**
+	 * Process the submitted config data
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function afterConfigSave(HookEvent $event) {
+		$tmpTemplates = wire('templates');
+
+		foreach($tmpTemplates as $template) { // exclude system templates
+			if($template->flags & Template::flagSystem) continue;
+			$templates[] = $template;
+		}
+
+		if($event->arguments(0) != $this) return;
+		$data = $event->arguments(1);
+		if(!empty($data['saas_templates'])) {
+			// Add saas_id field to selected templates
+			foreach($templates as $template) {
+				if(in_array($template->name, $data['saas_templates'])) {
+					if($template->hasField('saas_id')) {
+						continue;
+					} else {
+
+                        $template->fields->add('saas_id');
+						$template->fields->save();
+					}
+				} else {			
+					if($template->hasField('saas_id')) {
+						// remove SaaS field						
+                        $template->fields->remove('saas_id');
+						$template->fields->save();
+					} else {
+						continue;
+					}
+				}
+			}
+		}
+		$event->arguments(1, $data);
+	}
+
+	/**
+	 * Create saas_id field on install
+	 *
+	 */
+    public function ___install() {
+
+        // create saas_id field on user template
+        if(!$this->wire('fields')->saas_id) {
+            $f = new Field();
+            $f->type = 'FieldtypeInteger';
+			$f->inputfield = 'InputfieldInteger';
+			$f_name = 'saas_id';
+			$f->name = $f_name;
+			$f->label = $this->_('SaasiD to restrict access');
+            $f->description = $this->_('This is used by the SaaS module to limit this user to only see the branch starting with this parent when viewing the page list in the admin. It also restricts editing access to just this branch.');
+			try {
+				$f->save();
+			} catch(\Exception $e) {
+				$this->error("Error creating 'saas_id' field: " . $e->getMessage());
+			}
+
+            // Add the saas_id to all userTemplates
+            foreach($this->wire('config')->userTemplateIDs as $userTemplateId) {
+                $userTemplate = $this->wire('templates')->get($userTemplateId);
+                $userTemplate->fields->add($f);
+                $userTemplate->fields->save();
+            }
+
+        }
+    }
+
+	/**
+	 * Remove saas_id field from all templates on uninstall
+	 * Remove saas_id field on uninstall
+	 * 
+	 */
+    public function ___uninstall() {
+        $fields = wire('fields');
+        $templates = wire('templates');
+        $field = $this->wire('fields')->saas_id;
+
+        if($field) {
+            // remove the saas_id field from all usertemplates
+            foreach($this->wire('config')->userTemplateIDs as $userTemplateId) {
+                $userTemplate = $this->wire('templates')->get($userTemplateId);
+                $userTemplate->fields->remove($field);
+                $userTemplate->fields->save();
+            }
+            // remove the saas_id field from all templates
+			foreach($templates as $template) {
+				if(!$template->hasField($field)) continue;
+				$template->fields->remove($field);
+				$template->fields->save();
+            }
+            // remove saas_id field
+			$fields->delete($fields->get($field));
+        }
+    }
+
+
+}
